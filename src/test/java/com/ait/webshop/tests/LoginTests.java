ad1ff7bb1718003c3b8663403387bf06e9b6f3a0
Dietz7 <yuliya.arkhi@gmail.com>
2025-02-24 15:04:00 +0100

    modified LoginTests

--- src/test/java/com/ait/webshop/tests/LoginTests.java
+++ src/test/java/com/ait/webshop/tests/LoginTests.java
@@ -2,18 +2,22 @@ package com.ait.webshop.tests;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
-    @Test
-    public void loginPositiveTest() {
-        //click on Log in link
+
+    @BeforeMethod
+    public void login() {
         click(By.cssSelector("[href='/login']"));
 
         type(By.cssSelector("#Email"), "juli777@gmail.com");
 
         type(By.cssSelector("#Password"), "Qwerty1!$");
+    }
 
+    @Test
+    public void loginPositiveTest() {
         //Log in
         click(By.cssSelector(".login-button"));
         Assert.assertTrue(isElementPresent(By.cssSelector("a.ico-logout[href='/logout")));
