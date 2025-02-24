e63c53628b338b95a180074511d273c3038afe3e
Dietz7 <yuliya.arkhi@gmail.com>
2025-02-24 15:03:16 +0100

    modified AddItemToCartTests

--- src/test/java/com/ait/webshop/tests/AddItemToCartTests.java
+++ src/test/java/com/ait/webshop/tests/AddItemToCartTests.java
@@ -4,20 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.List;
 
 public class AddItemToCartTests extends TestBase {
-    @Test
-    public void loginPositiveTest() {
-        //click on Log in link
+
+    @BeforeMethod
+    public void login() {
         click(By.cssSelector("[href='/login']"));
 
         type(By.cssSelector("#Email"), "juli777@gmail.com");
 
         type(By.cssSelector("#Password"), "Qwerty1!$");
+    }
+
+    @Test
+    public void loginPositiveTest() {
 
         //Log in
         click(By.cssSelector(".login-button"));
@@ -29,23 +34,24 @@ public class AddItemToCartTests extends TestBase {
         Assert.assertTrue(isItemAddedByText("14.1-inch Laptop"));
 
         //Assert.assertTrue(isElementPresent(By.xpath("//td[@class='product']/a[contains(text(), '14.1-inch Laptop')]")),
-              //  "Product was not found in the cart");
+        //  "Product was not found in the cart");
 
     }
 
     @AfterMethod
-    public void postCondition(){
+    public void postCondition() {
 
         click(By.xpath("//tr[td[@class='product']]//input[@name='removefromcart']"));
 
         click(By.cssSelector("input[name='updatecart']"));
     }
-   public boolean isItemAddedByText(String text){
-       List<WebElement> items = driver.findElements(By.cssSelector("td.product a"));
-       for (WebElement element: items){
-           if(element.getText().contains(text)) return true;
-       }
-       return false;
-   }
+
+    public boolean isItemAddedByText(String text) {
+        List<WebElement> items = driver.findElements(By.cssSelector("td.product a"));
+        for (WebElement element : items) {
+            if (element.getText().contains(text)) return true;
+        }
+        return false;
+    }
 
 }
