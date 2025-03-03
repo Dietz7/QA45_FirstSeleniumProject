5935f159a1673479989e6685ef76a8c187b1705f
Dietz7 <yuliya.arkhi@gmail.com>
2025-03-03 14:24:29 +0100

    Updated CreateAccountTests

--- src/test/java/com/ait/webshop/tests/CreateAccountTests.java
+++ src/test/java/com/ait/webshop/tests/CreateAccountTests.java
@@ -1,32 +1,25 @@
 package com.ait.webshop.tests;
 
+import com.webshop.models.User;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CreateAccountTests extends TestBase {
-    @Test
+
+    
+    @Test()
     public void registerNewUserPositiveTest() {
         //click on Register link
-        click(By.cssSelector("[href='/register']"));
+        app.getUser().clickOnRegistrationLink();
         //enter First Name
-
         //better to use id - IDs are faster and more stable, and the element has a unique ID
         // OR driver.findElement(By.xpath("//*[@id='FirstName']")); good for hierarchical or complex elements searches
-        type(By.cssSelector("#FirstName"), "Juli");
-
-
-        type(By.cssSelector("#LastName"), "Tester");
-
-        type(By.cssSelector("#Email"), "juli777@gmail.com");
-
-        type(By.cssSelector("#Password"), "Qwerty1!$");
-
-        type(By.cssSelector("#ConfirmPassword"), "Qwerty1!$");
+        app.getUser().fillRegistrationForm(new User().setFirstName("Juli").setLastName("Tester").setEmail("juli777@gmail.com").
+                setPassword("Qwerty1!$"));
         //click on Registration button
-        click(By.cssSelector("[name='register-button']"));
-
-        Assert.assertFalse(isElementPresent(By.cssSelector(".login-button")));
-        Assert.assertTrue(isElementPresent(By.cssSelector(".account")));
+        app.getUser().confirmRegistration();
+        Assert.assertFalse(app.getUser().isElementPresent(By.cssSelector(".login-button")));
+       // Assert.assertFalse(app.getUser().isAccountPresent());
     }
 }
